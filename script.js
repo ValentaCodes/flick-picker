840b6a4eb9d2f1577edf5a30e67fa60d0becce47
ValentaCodes <valentacodes@gmail.com>
2022-12-07 01:55:51 -0700

    added function to display recently searched movies from local storage DEC 7 2022

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let searchButton = document.getElementById("searchButton"); //Gets search button from HTML
 let previousMoviePoster = [];
+let previousSearchedMovies = [];
 
-// This Api gets user data from Search input and returns JSON data
+// This Api gets user data from Search input and returns Youtube data
 let getYoutubeData = () => {
   let youtubeApiKey = "&key=AIzaSyAmVkvs3VVUOiXnF5hL7XGCk5kWBsxNLBY"; //Youtube API Key
   let youtubeApiInformation = "trailer&type=video&order=relevance&maxResults=1"; // API parameters that allows use to receive most relevant title
@@ -24,7 +25,7 @@ let getYoutubeData = () => {
     });
 };
 
-// function that gets needed data from OMDB
+// This Api gets needed data from OMDB
 let getOmdbData = () => {
   let apiUrl = "https://www.omdbapi.com/?t="; //apiURL with title parameter at the end
   let OmdbApiKey = "&apikey=749bae52"; // apiKey
@@ -44,7 +45,6 @@ let getOmdbData = () => {
           let criticRatings = data.Ratings[i].Value;
           console.log(`${ratingSource} - ${criticRatings}`); // this console.log shows us ratings and source
         }
-
 
         // these statements compare rotten tomatoes data and displays our flick suggestion
         let rottenTomatoRating = data.Ratings[1].Value; //grabs rotten data
@@ -101,29 +101,35 @@ let getOmdbData = () => {
         // sets url poster data in localStorage
         previousMoviePoster.push(data.Poster);
         localStorage.setItem("poster", JSON.stringify(previousMoviePoster));
-        // console.log(previousMoviePoster)
+
+        // sets movie title into storage
+        previousSearchedMovies.push(data.Title);
+        localStorage.setItem("movie", JSON.stringify(previousSearchedMovies));
       };
       grabMovieRatings();
       grabMovieData();
+      grabPreviousMoviePoster();
     });
 };
 
-const poster = JSON.parse(localStorage.getItem("poster")) || [];
+// grabs poster from storage
+let grabPreviousMoviePoster = () => {
+  let poster = JSON.parse(localStorage.getItem("poster"));
+  let imageEl = document.getElementsByTagName("img");
+  imageEl[0].setAttribute("src", poster.at(-1));
+  imageEl[1].setAttribute("src", poster.at(-2));
+  imageEl[2].setAttribute("src", poster.at(-3));
+};
+// grabs movie title from storage
+const movie = JSON.parse(localStorage.getItem("movie")) || [];
 
 // posterEl[1].setAttribute('src', )
 // sets previous user input in localStorage
-let setMovieHistory = () => {
-  let previousSearchedMovies = [{ movie: movieSearch.value }];
-  localStorage.setItem("movie", JSON.stringify(movie));
-  movie.push(previousSearchedMovies);
-};
-const movie = JSON.parse(localStorage.getItem("movie")) || [];
 
 // when search button is clicked fires API call functions
 searchButton.addEventListener("click", function () {
   getYoutubeData();
   getOmdbData();
-  setMovieHistory();
   // getNytData();
 });
 
